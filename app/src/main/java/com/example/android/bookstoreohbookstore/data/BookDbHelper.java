c5b31d3b82f5855454dfdf563ce6289f4227fc5f
Theda Rogers <crayonwriter@gmail.com>
2018-09-11 02:20:57 -0400

    create BookDbHelper class

--- app/src/main/java/com/example/android/bookstoreohbookstore/data/BookDbHelper.java
+++ app/src/main/java/com/example/android/bookstoreohbookstore/data/BookDbHelper.java
@@ -1,4 +1,32 @@
 package com.example.android.bookstoreohbookstore.data;
 
-public class BookDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BookDbHelper extends SQLiteOpenHelper {
+        public static final String DATABASE_NAME = "bookstore.db";
+        public static final int DATABASE_VERSION = 1;
+
+        public BookDbHelper (Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + BookContract.BookEntry.TABLE_NAME + " ("
+                + BookContract.BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BookContract.BookEntry.COLUMN_BOOK_TITLE + " TEXT NOT NULL, "
+                + BookContract.BookEntry.COLUMN_BOOK_PRICE + " INTEGER, "
+                + BookContract.BookEntry.COLUMN_BOOK_SUPPLIER_NAME + " TEXT NOT NULL, "
+                + BookContract.BookEntry.COLUMN_BOOK_SUPPLIER_PHONE + " TEXT);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
