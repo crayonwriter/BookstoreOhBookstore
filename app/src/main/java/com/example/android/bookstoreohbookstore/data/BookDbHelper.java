75f8b5d84ebcf689ea7be510e68f6b436fb64d7f
Theda Rogers <crayonwriter@gmail.com>
2018-09-12 17:45:41 -0400

    update: minor modifications to clean up contract and dbhelper

--- app/src/main/java/com/example/android/bookstoreohbookstore/data/BookDbHelper.java
+++ app/src/main/java/com/example/android/bookstoreohbookstore/data/BookDbHelper.java
@@ -3,6 +3,7 @@ package com.example.android.bookstoreohbookstore.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.bookstoreohbookstore.data.BookContract.BookEntry;
 
 public class BookDbHelper extends SQLiteOpenHelper {
         public static final String DATABASE_NAME = "bookstore.db";
@@ -14,15 +15,16 @@ public class BookDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + BookContract.BookEntry.TABLE_NAME + " ("
-                + BookContract.BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + BookContract.BookEntry.COLUMN_BOOK_TITLE + " TEXT NOT NULL, "
-                + BookContract.BookEntry.COLUMN_BOOK_PRICE + " INTEGER, "
-                + BookContract.BookEntry.COLUMN_BOOK_SUPPLIER_NAME + " TEXT NOT NULL, "
-                + BookContract.BookEntry.COLUMN_BOOK_SUPPLIER_PHONE + " TEXT);";
+        String SQL_CREATE_BOOKS_TABLE =  "CREATE TABLE " + BookEntry.TABLE_NAME + " ("
+                + BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BookEntry.COLUMN_BOOK_TITLE + " TEXT NOT NULL, "
+                + BookEntry.COLUMN_BOOK_PRICE + " INTEGER, "
+                + BookEntry.COLUMN_BOOK_SUPPLIER_NAME + " TEXT, "
+                + BookEntry.COLUMN_BOOK_QUANTITY + " INTEGER, "
+                + BookEntry.COLUMN_BOOK_SUPPLIER_PHONE + " TEXT);";
 
         // Execute the SQL statement
-        db.execSQL(SQL_CREATE_PETS_TABLE);
+        db.execSQL(SQL_CREATE_BOOKS_TABLE);
     }
 
     @Override
