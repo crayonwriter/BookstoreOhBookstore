0b1bacf8636446ae237c706b1cb9518f2415fe72
Theda Rogers <crayonwriter@gmail.com>
2018-09-14 19:34:23 -0400

    update: reformat and rearrange code

--- app/src/main/java/com/example/android/bookstoreohbookstore/data/BookDbHelper.java
+++ app/src/main/java/com/example/android/bookstoreohbookstore/data/BookDbHelper.java
@@ -3,25 +3,26 @@ package com.example.android.bookstoreohbookstore.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import com.example.android.bookstoreohbookstore.data.BookContract.BookEntry;
 
 public class BookDbHelper extends SQLiteOpenHelper {
-        public static final String DATABASE_NAME = "bookstore.db";
-        public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "bookstore.db";
+    public static final int DATABASE_VERSION = 1;
 
-        public BookDbHelper (Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
+    public BookDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String SQL_CREATE_BOOKS_TABLE = "CREATE TABLE " + BookEntry.TABLE_NAME + " ("
-            + BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + BookEntry.COLUMN_BOOK_TITLE + " TEXT NOT NULL, "
-            + BookEntry.COLUMN_BOOK_PRICE + " INTEGER, "
-            + BookEntry.COLUMN_BOOK_SUPPLIER_NAME + " TEXT, "
-            + BookEntry.COLUMN_BOOK_SUPPLIER_PHONE + " TEXT, "
-            + BookEntry.COLUMN_BOOK_QUANTITY + " INTEGER);";
+                + BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BookEntry.COLUMN_BOOK_TITLE + " TEXT NOT NULL, "
+                + BookEntry.COLUMN_BOOK_PRICE + " INTEGER, "
+                + BookEntry.COLUMN_BOOK_SUPPLIER_NAME + " TEXT, "
+                + BookEntry.COLUMN_BOOK_SUPPLIER_PHONE + " TEXT, "
+                + BookEntry.COLUMN_BOOK_QUANTITY + " INTEGER);";
 
         // Execute the SQL statement
         db.execSQL(SQL_CREATE_BOOKS_TABLE);
