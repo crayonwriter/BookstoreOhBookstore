3729d645e317a025911a27632ecc9285e79bf61c
Theda Rogers <crayonwriter@gmail.com>
2018-09-16 23:17:54 -0400

    feat: add urimatcher to contentprovider

--- app/src/main/java/com/example/android/bookstoreohbookstore/data/BookProvider.java
+++ app/src/main/java/com/example/android/bookstoreohbookstore/data/BookProvider.java
@@ -3,6 +3,7 @@ package com.example.android.bookstoreohbookstore.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -10,6 +11,28 @@ import android.net.Uri;
  * {@link ContentProvider} for bookstore app.
  */
 public class BookProvider extends ContentProvider {
+
+    /** URI matcher code for the content URI for the books table */
+    private static final int BOOKS = 100;
+
+    /** URI matcher code for the content URI for a single book in the books table */
+    private static final int BOOK_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+    sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS, BOOKS);
+    sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS + "#", BOOK_ID);
+    }
 
     //Global database helper object
     private BookDbHelper mBookDbHelper;
